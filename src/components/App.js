bba6298689adda2ceec5ad416abc6eec083bb592
Emmanuel Bravo <emmanuel@Emmanuels-MacBook-Pro.local>
2018-10-02 14:54:56 -0500

    completed including black diamond

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Post from './Post/Post';
 
 import './App.css';
 
@@ -10,40 +12,53 @@ class App extends Component {
     super();
 
     this.state = {
-      posts: []
+      posts: [],
+      input:''
     };
 
     this.updatePost = this.updatePost.bind( this );
     this.deletePost = this.deletePost.bind( this );
     this.createPost = this.createPost.bind( this );
+    this.changeHandler=this.changeHandler.bind(this);
   }
   
+
   componentDidMount() {
-
+    axios.get('https://practiceapi.devmountain.com/api/posts').then(response=>this.setState({posts:response.data}));
+    
+    
+    
   }
-
-  updatePost() {
+changeHandler(e){
+  this.setState({input:e.target.value});
+  console.log(this.state.input);
   
+}
+
+  updatePost(id,text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`,{text}).then(results=>this.setState({posts: results.data}))
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?${id}`).then(results=>this.setState({posts:results.data}))
   }
 
-  createPost() {
-
+  createPost(text) {
+    axios.post(`https://practiceapi.devmountain.com/api/posts`,{text}).then(results=>this.setState({posts:results.data}))
   }
 
   render() {
+    // console.log(this.state.posts)
     const { posts } = this.state;
 
     return (
       <div className="App__parent">
-        <Header />
+        <Header changeHandler={this.changeHandler}/>
 
         <section className="App__content">
 
-          <Compose />
+          <Compose createPostFn={this.createPost}/>
+          {this.state.input ? posts.filter(elem=>elem.text.includes(this.state.input)).map(elem=><Post deletePostFn={this.deletePost} updatePostFn={this.updatePost} key={elem.id} date={elem.date} text={elem.text} id={elem.id}></Post>) : posts.map(elem=><Post deletePostFn={this.deletePost} updatePostFn={this.updatePost} key={elem.id} date={elem.date} text={elem.text} id={elem.id}></Post>)}
           
         </section>
       </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -22,7 +22,7 @@ export default class Header extends Component {
 
           {/* Displays the search bar */}
           <div className="Header__right">
-            <Search />
+            <Search changeHandler={this.props.changeHandler}/>
 
             {/* Displays the profile icon */}
             <div className="Header__profile">
